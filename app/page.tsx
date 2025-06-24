b7f66d8f3e258ffd555d4e6b6ecbe47fe9beaa6f
Microwrk <microwrk.online@gmail.com>
2025-06-24 19:32:53 +0530

    added analytics

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,21 @@ import { SubscribeForm } from "@/components/subscribe-form";
 import { BlogList } from "@/components/blog-list";
 import { getSortedPostsData } from "@/lib/blog";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function HomePage() {
   const posts = getSortedPostsData();
 
   return (
-    <main className="min-h-screen">
-      <Header />
-      <HeroSection />
-      <SubscribeForm />
-      <BlogList posts={posts} />
-      <Footer />
-    </main>
+    <>
+      <Analytics />
+      <main className="min-h-screen">
+        <Header />
+        <HeroSection />
+        <SubscribeForm />
+        <BlogList posts={posts} />
+        <Footer />
+      </main>
+    </>
   );
 }
